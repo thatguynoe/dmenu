afecb6593228c51415d1ab988e7b97e9ead8505b
thatguynoe <noereyesrivas@gmail.com>
2022-09-18 21:54:18 -0400

    apply the grid patch

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ An almost vanilla build of [dmenu](https://tools.suckless.org/dmenu/).
 ## Patches and features
 
 * [**border-20201112-1a13d04**](https://tools.suckless.org/dmenu/patches/border/dmenu-border-20201112-1a13d04.diff) adds a border around the dmenu window of width `n` with the `-bw n` flag.
+* [**grid-4.9**](https://tools.suckless.org/dmenu/patches/grid/dmenu-grid-4.9.diff) allows you to render dmenu's entries in a grid with the `-g` and `-l` flags.
+    * [**gridnav-5.0**](https://tools.suckless.org/dmenu/patches/gridnav/dmenu-gridnav-5.0.diff) adds the ability to move left and right through a grid.
 
 ## Installation
 
--- dmenu.c
+++ dmenu.c
@@ -84,7 +84,7 @@ calcoffsets(void)
 	int i, n;
 
 	if (lines > 0)
-		n = lines * bh;
+		n = lines * columns * bh;
 	else
 		n = mw - (promptw + inputw + TEXTW("<") + TEXTW(">"));
 	/* calculate which items will begin the next page and previous page */
@@ -169,9 +169,15 @@ drawmenu(void)
 	}
 
 	if (lines > 0) {
-		/* draw vertical list */
-		for (item = curr; item != next; item = item->right)
-			drawitem(item, x, y += bh, mw - x);
+		/* draw grid */
+		int i = 0;
+		for (item = curr; item != next; item = item->right, i++)
+			drawitem(
+				item,
+				x + ((i / lines) *  ((mw - x) / columns)),
+				y + (((i % lines) + 1) * bh),
+				(mw - x) / columns
+			);
 	} else if (matches) {
 		/* draw horizontal list */
 		x += inputw;
@@ -328,6 +334,8 @@ keypress(XKeyEvent *ev)
 	int len;
 	KeySym ksym;
 	Status status;
+	int i, offscreen = 0;
+	struct item *tmpsel;
 
 	len = XmbLookupString(xic, ev, buf, sizeof buf, &ksym, &status);
 	switch (status) {
@@ -377,10 +385,52 @@ keypress(XKeyEvent *ev)
 			                  utf8, utf8, win, CurrentTime);
 			return;
 		case XK_Left:
+			if (columns > 1) {
+				if (!sel)
+					return;
+				tmpsel = sel;
+				for (i = 0; i < lines; i++) {
+					if (!tmpsel->left || tmpsel->left->right != tmpsel) {
+						if (offscreen)
+							break;
+						return;
+					}
+					if (tmpsel == curr)
+						offscreen = 1;
+					tmpsel = tmpsel->left;
+				}
+				sel = tmpsel;
+				if (offscreen) {
+					curr = prev;
+					calcoffsets();
+				}
+				break;
+			}
 		case XK_KP_Left:
 			movewordedge(-1);
 			goto draw;
 		case XK_Right:
+			if (columns > 1) {
+				if (!sel)
+					return;
+				tmpsel = sel;
+				for (i = 0; i < lines; i++) {
+					if (!tmpsel->right ||  tmpsel->right->left != tmpsel) {
+						if (offscreen)
+							break;
+						return;
+					}
+					tmpsel = tmpsel->right;
+					if (tmpsel == next)
+						offscreen = 1;
+				}
+				sel = tmpsel;
+				if (offscreen) {
+					curr = next;
+					calcoffsets();
+				}
+				break;
+			}
 		case XK_KP_Right:
 			movewordedge(+1);
 			goto draw;
@@ -738,9 +788,13 @@ main(int argc, char *argv[])
 		} else if (i + 1 == argc)
 			usage();
 		/* these options take one argument */
-		else if (!strcmp(argv[i], "-l"))   /* number of lines in vertical list */
+		else if (!strcmp(argv[i], "-g")) {   /* number of columns in grid */
+			columns = atoi(argv[++i]);
+			if (lines == 0) lines = 1;
+		} else if (!strcmp(argv[i], "-l")) { /* number of lines in grid */
 			lines = atoi(argv[++i]);
-		else if (!strcmp(argv[i], "-m"))
+			if (columns == 0) columns = 1;
+		} else if (!strcmp(argv[i], "-m"))
 			mon = atoi(argv[++i]);
 		else if (!strcmp(argv[i], "-p"))   /* adds prompt to left of input field */
 			prompt = argv[++i];
